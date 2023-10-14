e10ef02efd2d295edc6f1697018548d1e5e44d71
Paulo <paulosouzap7@gmail.com>
2023-10-14 16:50:54 -0300

    feat: json

---
+++ fundamentos/json/exemple.js
@@ -0,0 +1,11 @@
+const book1 = {
+    name: "Recatoring",
+    author: "Martin Fowler"
+};
+
+const book2 = {
+    name: "Refactoring",
+    author: "Martin Fowler"
+}
+
+console.log(book1 === book2)
